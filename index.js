bd8b3bc8c80c8cecdad351ae6c08fa3aff71a0b0
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-04-28 00:34:10 +0600

    user id password secured

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config()
 const port=process.env.PORT || 5000;
 
-  //const uri = "mongodb+srv://<username>:<password>@cluster0.pflyccd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+ 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-//RkDHr9NIovEZX4n9
-//ArabVoyage
 
-const uri = "mongodb+srv://ArabVoyage:RkDHr9NIovEZX4n9@cluster0.pflyccd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+
+ 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pflyccd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
